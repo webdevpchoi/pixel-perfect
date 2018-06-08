b035bda3efd7520d699c5b8a4bf7c7a17184eca0
WonkasWilly <pchoi13370@gmail.com>
2018-06-08 13:23:14 -0700

    added in post and get routes, and passed in data

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const 	express = require('express'),
+		mongoose = require('mongoose'),
+		bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 //connect to db
@@ -15,26 +18,47 @@ db.once('open', function() {
 	console.log('DB succesfully connected!');
 })
 //create mongoose schema
-const locationSchema = mongoose.Schema({
-	name: String
+const modelSchema = mongoose.Schema({
+	name: String,
+	image: String
 })
 //compile schema into model so it had methods available to maniuplate data
 //first argument of model is the singular name of the collection your model is for
-const Location = mongoose.model('Location', locationSchema);
+const Model = mongoose.model('Model', modelSchema);
 
-const happyPlace = new Location({
-	name: "The Happy Place",
-})
 
-happyPlace.save(function(err, happyPlace) {
-	if(err) {
-		console.log('This didn\'t save to the DB!')
-	} else {
-		console.log(happyPlace);
-	}
-})
 app.get('/', (req, res) => res.render('landing'));
 
+app.get('/models', (req, res) => {
+		Model.find({}, (err, models) => {
+		if(err) {
+			console.log('Could not find the models');
+		} else {
+			res.render("models", {models: models})
+		}
+	})
+});
+
+app.post('/models', (req, res) => {
+	const name = req.body.name;
+	const imageUrl = req.body.image;
+
+	const model = new Model({
+		name: name,
+		image: imageUrl,
+	})
+
+	model.save((err, model) => {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log(model);
+		}
+	})
+
+})
+
+//listening to port
 app.listen(port, () => {
 	console.log('The server is up and running!');
 });
