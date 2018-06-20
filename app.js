de3238f01452d95ccc57f2321f619f3a4fc4ad4b
WonkasWilly <pchoi13370@gmail.com>
2018-06-20 16:08:42 -0700

    added login and signup routes

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const 	express = require('express'),
-		mongoose = require('mongoose'),
+const 	LocalStrategy = require('passport-local'),
+		Comment = require('./schemas/comment'),
+		session = require('express-session'),
 		bodyParser = require('body-parser'),
 		Model = require('./schemas/model'),
-		Comment = require('./schemas/comment'),
+		User = require('./schemas/user'),
+		mongoose = require('mongoose'),
+		passport = require('passport'),
+	 	express = require('express'),
 		seedDB = require('./seeds');
 
 const app = express();
@@ -11,6 +15,17 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+
+app.use(require("express-session")({
+    secret: "Beauty is in the eye of the beholder.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //connect to db
 mongoose.connect('mongodb://localhost/tgr');
@@ -71,6 +86,37 @@ app.post('/models', (req, res) => {
 	})
 
 })
+
+//auth routes
+app.get('/login', (req, res) => {
+	res.render('auth/login');
+})
+
+app.post('/login', passport.authenticate('local', 
+{
+	successRedirect: '/',
+	failureRedirect:'/login',
+}), (req, res) => {
+});
+
+app.get('/signup', (req, res) => {
+	res.render('auth/signup');
+})
+
+app.post('/signup', (req, res) => {
+	const newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, (err, user) => {
+		if(err) {
+			console.log(err);
+			return res.render('signup');
+		} else {
+			passport.authenticate('local')(req, res, function() {
+				console.log('user was created and authenticated!');
+				res.redirect('/');
+			})
+		}
+	});
+});
 
 //get route for new comments
 app.get('/models/:id/comments/new', (req, res) => {
