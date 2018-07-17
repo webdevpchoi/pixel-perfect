f9921f9954c9c74844302f1ae4e4abae1d2ffb3e
WonkasWilly <pchoi13370@gmail.com>
2018-07-17 15:38:16 -0700

    readded then removed seeds file

--- seeds.js
+++ seeds.js
@@ -12,25 +12,25 @@ const data = [
 		name: "Melissa",
 		image: "https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 		desc: "Modeling for only 2 years, Melissa has mastered it all within a short time period.",
-		creator:{}
+		creator:{},
 	},
 	{
 		name: "John Smith",
 		image: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&h=350",
 		desc: "Despite his unoriginal name, John makes a mark on modeling history with his signature dead-eyed-mackeral facial expressions",
-		creator:{}
+		creator:{},
 	},
 	{
 		name: "John Park",
 		image: "https://images.pexels.com/photos/450214/pexels-photo-450214.jpeg?auto=compress&cs=tinysrgb&h=350",
 		desc: "A sea of Asians could surround this particular model, and he'd STILL stand out. That's how good he is!",
-		creator:{}
+		creator:{},
 	},
 	{
 		name: "Ashley Sins",
 		image: "https://images.pexels.com/photos/301298/pexels-photo-301298.jpeg?auto=compress&cs=tinysrgb&h=350",
 		desc: "She might only have 10/10 vision, but Ashley's singature bangs gives her a signature BANG.",
-		creator:{}
+		creator:{},
 	}
 ]
 
@@ -40,7 +40,7 @@ const samplePhotographers = [
 		desc: "I take photos of stuff",
 		img: "https://images.pexels.com/photos/108148/pexels-photo-108148.jpeg?auto=compress&cs=tinysrgb&h=350",
 		specialty: "Street Photography",
-		creator: {}
+		creator: {},
 	},
 
 	{
@@ -48,14 +48,14 @@ const samplePhotographers = [
 		desc: "I enjoy the fresh air, and nature's beauty.",
 		img: "https://images.pexels.com/photos/610293/pexels-photo-610293.jpeg?auto=compress&cs=tinysrgb&h=350",
 		specialty: "Outdoor Photography",
-		creator: {}
+		creator: {},
 	},
 	{
 		name: "Jane",
 		desc: "Buildings are the the building blocks of civilization.",
 		img: "https://images.pexels.com/photos/139829/pexels-photo-139829.jpeg?auto=compress&cs=tinysrgb&h=350",
 		specialty: "Outdoor Photography",
-		creator: {}
+		creator: {},
 	},
 ]
 
@@ -82,68 +82,75 @@ function seedDB() {
 							console.log('Seeds File Error: Models were not removed.');
 						} else {
 							console.log('All Models Removed!')
-							User.findOne({username: 'John Test'}, (err, found) => {
+							//remove all current photographers in db
+							Photographer.remove({}, (err) => {
 								if(err) {
-									console.log(err);
+									console.log(err)
 								} else {
-									data.forEach((modelData) => {
-										//assign a test user id to model
-										modelData.creator.id = found._id;
-										console.log(modelData)
-										//create new models
-										Model.create(modelData, (err, model) => {
-											if(err) {
-												console.log(err);
-											} else {
-												console.log('A model was created!');
-												//create a comment for each model
-												Comment.create({
-													text: "This is a sample comment!",
-													author: johnTest.username,
-												}, (err, comment) => {
-													if(err) {
-														console.log(err);
-													} else {
-														model.comments.push(comment);
-														model.save();
-														console.log('New comment created!');
-													}
+									console.log('All photographers removed!');
+									//find the default user in db
+									User.findOne({username: 'John Test'}, (err, found) => {
+										if(err) {
+											console.log(err);
+										} else {
+												data.forEach((modelData) => {
+													//assign a reference from each model to default user
+													modelData.creator.id = found._id;
+													//create new models
+													Model.create(modelData, (err, model) => {
+														if(err) {
+															console.log(err);
+														} else {
+															console.log('A model was created!');
+															//create a comment for each model
+															Comment.create({
+																text: "This is a sample comment!",
+																author: johnTest.username,
+															}, (err, comment) => {
+																if(err) {
+																	console.log(err);
+																} else {
+																	comment.author.id = createdUser._id;
+																	comment.author.username = createdUser.username;
+																	comment.save();
+																	model.comments.push(comment);
+																	model.save();
+																	console.log('New comment created!');
+																}
+															})
+														}
+													})
 												})
-											}
-										})
+												samplePhotographers.forEach((photographer) => {
+													//create new models
+													Photographer.create(photographer, (err, photographer) => {
+														if(err) {
+															console.log(err);
+														} else {
+															console.log(`Added photographer ${photographer.name}`);
+															Comment.create({
+																text: "This is the default comment for the PHOTOGRAPHER.",
+																author: {
+																	username: johnTest.username,
+																},
+															}, (err, newComment) => {
+																if(err) {
+																	console.log(err)
+																} else {
+																	console.log(`Comment added for photographer`);
+																	newComment.author.id = createdUser._id;
+																	newComment.author.username = createdUser.username;
+																	newComment.save();
+																	photographer.comments.push(newComment);
+																	photographer.save();
+																}
+															})
+														}
+													})
+												})
+										}
 									})
 								}
-							})
-						}
-					})
-
-					Photographer.remove({}, (err) => {
-						if(err) {
-							console.log(err)
-						} else {
-							console.log('All photographers removed!');
-							samplePhotographers.forEach((photographer) => {
-								Photographer.create(photographer, (err, photographer) => {
-									if(err) {
-										console.log(err)
-									} else {
-										console.log(`Added photographer ${photographer.name}`);
-										Comment.create({
-											text: "This is the default comment for the PHOTOGRAPHER.",
-											author: {
-												username: "Generic User",
-											},
-										}, (err, newComment) => {
-											if(err) {
-												console.log(err)
-											} else {
-												console.log(`Comment added for photographer`);
-												photographer.comments.push(newComment);
-												photographer.save();
-											}
-										})
-									}
-								})
 							})
 						}
 					})
